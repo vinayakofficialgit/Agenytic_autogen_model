a99de7c554cb84ce52f8915ca39322c521ffd76f
Vinayak Sharma (IND_PUN) <Vinayak.Sharma@opustechglobal.com>
2026-02-09 17:12:35 +0530

    application added\ files

---
+++ Order-app-main/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Alertm from './Components/Alertm';
+import Homepage from './Pages/Homepage';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+  
+// } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+     <Homepage/>
+     <Alertm/>
+
+    </div>
+  );
+}
+
+export default App;
